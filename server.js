e53c72c8f2f3d2bf3f42e760de1066e81dfd0edf
sahil-1602 <sahildarji16@gmail.com>
2021-06-12 12:50:44 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const jwt = require('jsonwebtoken');
+app.use(express.json())
+
+const posts = [
+    {
+        username: 'sahil',
+        title: 'post 1'
+    },
+    {
+        username: 'kyle',
+        title: 'post 2'
+    }
+
+]
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
+
+
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3000)
+
+// require('crypto').randomBytes(64).toString('hex')
